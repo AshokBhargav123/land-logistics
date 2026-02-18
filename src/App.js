c86bc471d80f01814f4adce0950d70c19bd3b72e
AshokBhargav123 <pashokbhargav6@gmail.com>
2026-02-18 17:03:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from "./components/HeroSection";
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/images.js
@@ -0,0 +1,12 @@
+import img1 from "../assets/images/industry.jpeg";
+import img2 from "../assets/images/industry-2.jpg";
+import img3 from "../assets/images/industry-3.jpg";
+import img4 from "../assets/images/industry-4.jpg";
+import img5 from "../assets/images/industry-5.jpg";
+import img6 from "../assets/images/industry-6.jpg";
+import img7 from "../assets/images/industry-7.avif";
+import img8 from "../assets/images/industry-8.jpg";
+
+export const column1 = [img1, img4, img7];
+export const column2 = [img2, img5];
+export const column3 = [img3, img6, img8];
